267eb22056df129eed7948e7e6e4aeb6bdc5d94b
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-01-23 23:24:42 +0000

    6th excercises corrected, 7th excercise done

--- 7th_excercise.sh
+++ 7th_excercise.sh
@@ -3,7 +3,9 @@
 #Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file.
 #Also perform an ls command against the file or directory with the long listing option.
 
-read -p "What is the name of your file or directory?" file_name
+file_name="file.txt"
+
+#Modify the previous script to that it accepts the file or directory name as an argument instead of prompting the user to enter it.
 
 if [ -e $file_name ]; then
   echo "$file_name exists"
--- main.sh
+++ main.sh
@@ -3,9 +3,8 @@
 #Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file.
 #Also perform an ls command against the file or directory with the long listing option.
 
-read -p "What is the name of your file or directory?" file_name
-
 file_name="file.txt"
+
 if [ -e $file_name ]; then
   echo "$file_name exists"
 else
@@ -23,3 +22,5 @@ if [ -d $file_name ]; then
 else
   echo "$file_name is not a directory"
 fi
+
+#Modify the previous script to that it accepts the file or directory name as an argument instead of prompting the user to enter it.
