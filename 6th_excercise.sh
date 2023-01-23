370bda462805d98f166333e8ae0588101c8365a1
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-01-23 23:13:56 +0000

    6th excercise solved

--- 6th_excercise.sh
+++ 6th_excercise.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
-#Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
+#Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file.
+#Also perform an ls command against the file or directory with the long listing option.
 
+read -p "What is the name of your file or directory?" file_name
+
+file_name="file.txt"
+if [ -e $file_name ]; then
+  echo "$file_name exists"
+else
+  echo "$file_name does not exist"
+fi
+
+if [ -f $file_name ]; then
+  echo "$file_name is a regular file"
+else
+  echo "$file_name is not a regular file"
+fi
+
+if [ -d $file_name ]; then
+  echo "$file_name is a directory"
+else
+  echo "$file_name is not a directory"
+fi
