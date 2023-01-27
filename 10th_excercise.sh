8af6530fec8efce16e99964112bfb1da33192dbc
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-01-27 22:12:26 +0000

    10th_excercise complete

--- 10th_excercise.sh
+++ 10th_excercise.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-#Write a shell script that accepts a file or directory name as an argument. Have the script report if it is reguler file, a directory, or another type of file. If it is a directory, exit with a 1 exit status. If it is some other type of file, exit with a 2 exit status.
+#Write a shell script that accepts a file or directory name as an argument. 
+#Have the script report if it is reguler file, a directory, or another type of file. If it is a directory, exit with a 1 exit status. If it is some other type of file, exit with a 2 exit status.
 
-file_or_directory=$1
+for file_name in "$@"; do
 
-if [ -e $file_name ]; then
-  echo "$file_name is a file"
+if [ -d $file_name ]; then
+  echo "$file_name is a directory"
+exit 1
 else
-  echo "$file_name does not exist"
+  echo "$file_name is not a directory"
 fi
+
+if [ -f $file_name ]; then
+  echo "$file_name is a regular file"
+else
+  echo "$file_name is not a regular file"
+exit 2
+fi
+done
--- main.sh
+++ main.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-#Write a shell script that accepts a file or directory name as an argument. Have the script report if it is reguler file, a directory, or another type of file. If it is a directory, exit with a 1 exit status. If it is some other type of file, exit with a 2 exit status.
+#Write a shell script that accepts a file or directory name as an argument. 
+#Have the script report if it is reguler file, a directory, or another type of file. If it is a directory, exit with a 1 exit status. If it is some other type of file, exit with a 2 exit status.
 
-file_or_directory=$1
+for file_name in "$@"; do
 
-if [ -e $1 ]; then
-  echo "$1 is a file"
+if [ -d $file_name ]; then
+  echo "$file_name is a directory"
+exit 1
 else
-  echo "$1 does not exist"
+  echo "$file_name is not a directory"
 fi
 
-if [ -d $1 ]; then
- exit 1
+if [ -f $file_name ]; then
+  echo "$file_name is a regular file"
+else
+  echo "$file_name is not a regular file"
+exit 2
 fi
+done
