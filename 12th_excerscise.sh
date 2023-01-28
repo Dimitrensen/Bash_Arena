87813198dcc8ac9dc4043eccdec393f190e5e501
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-01-28 14:35:57 +0000

    12th excercise done but the script is wrong

--- 12th_excerscise.sh
+++ 12th_excerscise.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #Write a shell script that consists of a function that displays the number of files in the present working directory. Name this function “file_count” and call it in your script. If you use variable in your function, remember to make it a local variable.
 
 for file_count in "main.sh"; do
