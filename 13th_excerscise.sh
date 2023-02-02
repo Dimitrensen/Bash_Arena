3da9a37ea4bcb8f44c6a771ffad362b5bd2b1205
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-02-02 13:11:57 +0000

    Excercise 13 completed

--- 13th_excerscise.sh
+++ 13th_excerscise.sh
@@ -1,3 +1,16 @@
-#!/usr/bin/env bash
+#Modify the script from the previous exercise. Make the “file_count” function accept a directory as an argument. 
+#Next, have the function display the name of the directory followed by a colon.
 
-#Modify the script from the previous exercise. Make the “file_count” function accept a directory as an argument. Next, have the function display the name of the directory followed by a colon. Finally display the number of files to the screen on the next line. Call the function three times. First on the “/etc” directory, next on the “/var” directory and finally on the “/usr/bin” directory.
+#Finally display the number of files to the screen on the next line. Call the function three times. First on the “/etc” directory, next on the “/var” directory and finally on the “/usr/bin” directory.
+
+file_count () {
+    local dir=$1
+    local count=$(ls -1 "$dir" | wc -l)
+    echo "Number of files in the $dir: $count"
+}
+
+file_count /etc
+file_count /var
+file_count /usr/bin
+
+echo "$(pwd)/"
--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
-#Modify the script from the previous exercise. Make the “file_count” function accept a directory as an argument. Next, have the function display the name of the directory followed by a colon. Finally display the number of files to the screen on the next line. Call the function three times. First on the “/etc” directory, next on the “/var” directory and finally on the “/usr/bin” directory.
+#Modify the script from the previous exercise. Make the “file_count” function accept a directory as an argument. 
+#Next, have the function display the name of the directory followed by a colon.
+
+#Finally display the number of files to the screen on the next line. Call the function three times. First on the “/etc” directory, next on the “/var” directory and finally on the “/usr/bin” directory.
 
 file_count () {
     local dir=$1
@@ -9,3 +12,5 @@ file_count () {
 file_count /etc
 file_count /var
 file_count /usr/bin
+
+echo "$(pwd)/"
