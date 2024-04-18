630e4c8dd5fff11fccd6cfe1ee69ae8e83b52fef
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2024-04-18 15:27:15 +0000

    Vanilla script 90% ready

--- json_templating_script/json-template-simple.sh
+++ json_templating_script/json-template-simple.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Read the contents of base.json and extension.json
-base=$(<base.json)
-extension=$(<extension.json)
+# Read the contents of base.json and extension.json into variables
+base=$(cat base.json)
+extension=$(cat extension.json)
 
-result=$(jq --argjson base "$base" '. | tostring' extension.json)
+# Use jq to perform the template substitution
+result=$(echo "$extension" | jq --argjson base "$base" '.store = $base.store | .data.point = $base.a | .price = $base.d | .location.toc = $base.b.c | .target.f = $base.b')
 
 echo "$result" > result.json
