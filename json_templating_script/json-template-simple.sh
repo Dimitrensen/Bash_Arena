bd0a525aa1cfcb5016436d461e8bc2ffac35df19
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2024-04-21 19:42:46 +0000

    .store operation omitted

--- json_templating_script/json-template-simple.sh
+++ json_templating_script/json-template-simple.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Read the contents of base.json and extension.json into variables
+# Variables naming in accordance to json files
 base=$(cat base.json)
 extension=$(cat extension.json)
 
-# Use jq to perform the template substitution
-result=$(echo "$extension" | jq --argjson base "$base" '.store = $base.store | .data.point = $base.a | .price = $base.d | .location.toc = $base.b.c | .target.f = $base.b')
+result=$(echo "$extension" | jq --argjson base "$base" '{
+  store,
+  data: { point: $base.a },
+  price: $base.d,
+  location: { toc: $base.b.c },
+  target: { f: $base.b }
+}')
 
 echo "$result" > result.json
